75435ca9e11ee6741441d9fdb1392fba497137fc
Austin Keeler <akeeler@Austins-MacBook-Air.local>
2021-01-09 23:15:53 -0500

    Adding Another Branch

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -51,20 +51,26 @@ let baseMaps = {
 
 // // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satilliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 })
 
 //Acessing Toronto NeighborHoods from Github
 let torontoHoods = "https://raw.githubusercontent.com/austink24/Mapping_Earthquakes/main/torontoNeighborhoods.json"
 
-// Grab GeoJSON data from trontoHoods
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-  L.geoJson(data).addTo(map)
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
 
-})
+// // Grab GeoJSON data from trontoHoods
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+//   L.geoJson(data).addTo(map)
+
+// })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
