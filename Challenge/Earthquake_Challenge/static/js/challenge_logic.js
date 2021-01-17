a00d9390f5d7c1b41442dd956707fcef4e1dc075
Austin Keeler <akeeler@Austins-MacBook-Air.local>
2021-01-16 23:00:13 -0500

    Adding Layers

--- Challenge/Earthquake_Challenge/static/js/challenge_logic.js
+++ Challenge/Earthquake_Challenge/static/js/challenge_logic.js
@@ -31,11 +31,13 @@ let baseMaps = {
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 let tectonicData = new L.LayerGroup();
+let majorQuakes = new L.LayerGroup()
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
+  "Tectonic Plat Lines": tectonicData,
   "Earthquakes": allEarthquakes,
-  "Tectonic Plat Lines": tectonicData
+  "Major Earth Quakes": majorQuakes
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -93,7 +95,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   L.geoJson(data, {
     	// We turn each feature into a circleMarker on the map.
     	pointToLayer: function(feature, latlng) {
-      		console.log(data);
+      		//console.log(data);
       		return L.circleMarker(latlng);
         },
       // We set the style for each circleMarker using our styleInfo function.
@@ -129,7 +131,7 @@ legend.onAdd = function() {
 
 // Looping through our intervals to generate a label with a colored square for each interval.
   for (var i = 0; i < magnitudes.length; i++) {
-    console.log(colors[i]);
+    //console.log(colors[i]);
     div.innerHTML +=
       "<i style='background: " + colors[i] + "'></i> " +
       magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
@@ -143,8 +145,65 @@ legend.onAdd = function() {
 
   
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json('https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json').then(function(data1) {
-    console.log(data1)
-    L.geoJson(data1).addTo(map)
-  });
+d3.json('https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json').then(function(data) {
+    //console.log(data)
+    L.geoJson(data, {
+      color: 'Orange',
+      weight: '4'
+    }).addTo(tectonicData)
+  });tectonicData.addTo(map)
+
+  d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson').then(function(data) {
+    console.log('got data')
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+    }
+  
+    // This function determines the color of the marker based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+      if (magnitude > 5) {
+        return "#ea2c2c";
+      }
+      if (magnitude > 4) {
+        return "#ea822c";
+      }
+      if (magnitude < 4) {
+        return "#ee9c00";
+      }
+      return "#98ee00";
+    }
+  
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+    function getRadius(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+      return magnitude * 4;
+    }
+  
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        // We turn each feature into a circleMarker on the map.
+        pointToLayer: function(feature, latlng) {
+            //console.log(data);
+            return L.circleMarker(latlng);
+          },
+        // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo,
+       // We create a popup for each circleMarker to display the magnitude and location of the earthquake
+       //  after the marker has been created and styled.
+       onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(majorQuakes)
+  });majorQuakes.addTo(map)
 });
